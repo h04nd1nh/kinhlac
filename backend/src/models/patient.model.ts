d8ce25967ab0577efe201e29ee8774f3529aa1b7
trangtd <trangtruong.dig@gmail.com>
2026-03-20 21:16:46 +0700

    update: update code

--- backend/src/models/patient.model.ts
+++ backend/src/models/patient.model.ts
@@ -11,25 +11,25 @@ export class Patient {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar' })
-  fullName: string;
+  @Column({ type: 'varchar', nullable: true })
+  fullName: string | null;
 
-  @Column({ type: 'varchar', length: 10 })
-  gender: string;
+  @Column({ type: 'varchar', length: 10, nullable: true })
+  gender: string | null;
 
   @Column({ type: 'date', nullable: true })
   dateOfBirth: string | null;
 
-  @Column({ type: 'varchar', length: 10, nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   timeOfBirth: string | null;
 
   @Column({ type: 'text', nullable: true })
   address: string | null;
 
-  @Column({ type: 'varchar', length: 100, nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   province: string | null;
 
-  @Column({ type: 'varchar', length: 20, nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   phone: string | null;
 
   @Column({ type: 'text', nullable: true })
